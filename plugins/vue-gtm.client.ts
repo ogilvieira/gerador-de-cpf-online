88ac71848ff75d53700380a670d3311c1fb3fb30
Gilmair Vieira <ogilvieira@users.noreply.github.com>
2024-02-28 19:49:25 -0300

    Update vue-gtm.client.ts

--- plugins/vue-gtm.client.ts
+++ plugins/vue-gtm.client.ts
@@ -3,7 +3,7 @@ import { createGtm } from '@gtm-support/vue-gtm';
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(createGtm({
     id: 'GTM-NHBJHZ5',
-    defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+    defer: true, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
     compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
     nonce: '2726c7f26c', // Will add `nonce` to the script tag
     enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
